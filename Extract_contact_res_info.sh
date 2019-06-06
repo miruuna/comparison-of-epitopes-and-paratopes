cc45830c0b7b4663ee17cee272a71689c42050fb
Miruna Serian <zcbtser@ucl.ac.uk>
2019-06-06 15:17:00 +0100

    Create Extract_contact_res_info.sh

---
+++ Extract_contact_res_info.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+for file in ./pdb_structures/*.pdb; do
+    python v2.py --f1 ${file} --f2 ${file} --c1 AB --c2 C --c 4.5 --i 10.0 --jobid .\\$(basename ${file%.*})\\
+done
+
+for folder in ./out_/* ; do
+    for file in ./${folder}/*.txt; do
+        sed 's/[[:space:]]/,/g' ${file} > ${file}.csv
+    done
+done
+
+for folder in ./out_/* ; do
+    for file in ./${folder}/*.txt; do
+        find . -type f -name "parameters.txt.csv*" -print -delete
+
+    done
+done
