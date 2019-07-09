192a94cdfea804bf11c15af4846a31cfee8df7b3
Miruna Serian <zcbtser@ucl.ac.uk>
2019-07-09 18:21:06 +0100

    changed to generate different heatmaps

--- generate_contacts_heatmap.py
+++ generate_contacts_heatmap.py
@@ -9,40 +9,59 @@ pdb_codes1=["2eks"]
 pdb_codes= json.loads(open('pdb_codes.json').read())
 contact_residues= json.loads(open('contact_residues_all_imgtt.json').read())
 
-heavy_chain=[]
-for pdb in pdb_codes:
-    a_dict={}
-    try:
-        pdb_light_=[]
-        entry_list=[]
-        antigen = annotation[pdb]['antigen'][0]
-        antibody= annotation[pdb]['antibody']
+def get_contacts(chain, param):
+    contacts=[]
+    for pdb in pdb_codes:
+        a_dict={}
+        try:
+            pdb_light_=[]
+            entry_list=[]
+            antigen = annotation[pdb]['antigen'][0]
+            #antibody= annotation[pdb]['antibody']
+            if chain=="light":
+                if (elem=='L' for elem in annotation[pdb]['antibody']):
+                    antibody= annotation[pdb]['antibody'][1]
+                    non_h_cons=contact_residues[pdb][antigen][antibody][param]
 
-        non_h_cons=contact_residues[pdb][antigen][antibody[1]]['non_h_bonded']
-        res1_pos=[]
-        for elem in non_h_cons:
-            res1_pos.append(elem['res1_pos'])
-            no_elem=non_h_cons.count(elem)
-        dict={pos: int(res1_pos.count(pos)) for pos in res1_pos}
-        #heavy_chain[pdb]=(res1_pos)
-        #print(pdb, res1_pos)g
-        a_dict[pdb]=dict
-    except KeyError:
-        continue
-    heavy_chain.append(a_dict)
+                else:
+                    antibody= annotation[pdb]['antibody'][0]
+                    non_h_cons=contact_residues[pdb][antigen][antibody][param]
 
-df = pd.DataFrame.from_records([{'pdb': k, **v} for d in heavy_chain for k,v in d.items()])
-df.set_index('pdb',inplace=True)
-the_columns=list(df.columns)
-sorted_col=[]
-for elem in the_columns:
-    sorted_col.append(int(elem))
+            elif chain=="heavy":
+                if (elem=='H' for elem in annotation[pdb]['antibody']):
+                    antibody= annotation[pdb]['antibody'][0]
+                    non_h_cons=contact_residues[pdb][antigen][antibody][param]
+                elif (elem=='B' for elem in annotation[pdb]['antibody']):
+                    antibody= annotation[pdb]['antibody'][1]
+                    non_h_cons=contact_residues[pdb][antigen][antibody][param]
 
-df=df[natsorted(df.columns)]
+            else:
+                print("no chain given")
+            res1_pos=[]
+            for elem in non_h_cons:
+                res1_pos.append(elem['res1_pos'])
+                no_elem=non_h_cons.count(elem)
+            dict={pos: int(res1_pos.count(pos)) for pos in res1_pos}
+            #heavy_chain[pdb]=(res1_pos)
+            #print(pdb, res1_pos)g
+            a_dict[pdb]=dict
+        except KeyError:
+            continue
+        contacts.append(a_dict)
 
-print(df)
-print(natsorted(df.columns))
-fig, ax = plt.subplots(figsize=(60,20))
-g = sns.heatmap(df, xticklabels=True, yticklabels=True, ax=ax, linewidths=.1, annot=True, annot_kws={"size": 20}, cmap="YlGnBu", cbar=False)
-ax.tick_params(labelbottom='off',labeltop='on', labelsize=20)
-g.get_figure().savefig("heatmap.png")
+    df = pd.DataFrame.from_records([{'pdb': k, **v} for d in contacts for k,v in d.items()])
+    df.set_index('pdb',inplace=True)
+    df=df[natsorted(df.columns)]
+
+    fig, ax = plt.subplots(figsize=(60,30))
+    fig.autofmt_xdate()
+    plt.subplots_adjust(top=0.9) #adjust sublots labels to make room for title
+    plt.suptitle(chain+" chain: "+param, fontsize = 36) #title
+    g = sns.heatmap(df, xticklabels=True, yticklabels=True, ax=ax, linewidths=.1, annot=True, annot_kws={"size": 20}, cmap="YlGnBu", cbar=False)#.set_title(chain+" chain: "+param)
+    ax.tick_params(labelbottom='off',labeltop='on', labelsize=25)
+    g.get_figure().savefig("heatmap_"+chain+"_"+param+".png")
+
+get_contacts('light','h_bonded')
+get_contacts('heavy','h_bonded')
+get_contacts('light','non_h_bonded')
+get_contacts('heavy','non_h_bonded')
