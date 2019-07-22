55f06cf1a0f8d6885aff4b3ce588936808f9d095
Miruna Serian <zcbtser@ucl.ac.uk>
2019-07-22 18:25:52 +0100

    Update distribution_of_jaccard_scores.py

--- lysozyme/plots/distribution_of_jaccard_scores.py
+++ lysozyme/plots/distribution_of_jaccard_scores.py
@@ -1,25 +1,18 @@
 # Dependencies
-import collections
-from sklearn.metrics import jaccard_similarity_score
+
 import distance
 import matplotlib.pyplot as plt
 import json
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
-from natsort import natsorted
+
 
 species = json.loads(open("species_lysozime.json").read())
 d=json.loads(open('equiv_contacts_list_all_contacts.json').read())
 
-a={'a': [2 , 3, 4],
-   'b': [3,4,5],
-    'c':[4,1,3],
-   'd':[5,1,2]}
 
 p={}
-count=0
+
 key_list=[]
 for key, v in d.items():
     key_list.append(key)
@@ -33,22 +26,17 @@ for k in key_list:
                     d[i].append(0)
                 elif len(d[k])< len(d[i]):
                     d[k].append(0)
-            cos1+=(1-distance.jaccard(set(d[k]),set(d[i])))
+            cos1+=(1-distance.jaccard(set(d[k]),set(d[i]))) #Jaccard scores = 1-Jaccard distance
     p[k]=cos1/(len(key_list)-1)
 
 p_sorted={k: v for k, v in sorted(p.items(), key=lambda x: x[1])}
 name_list=[]
 score_list=[]
-species_list=[]
+
 for k, v in p_sorted.items():
     name_list.append(k)
     score_list.append(v)
-
-print(p)
-
-
-
-
+   
 data=(score_list, name_list)
 species_d=species_list
 colors = ("red", "green", "blue")
