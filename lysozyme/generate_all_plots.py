04500cbec4d7d97b2ea39bf7f5ade80bcb2bbe3a
Miruna Serian <zcbtser@ucl.ac.uk>
2019-08-18 01:31:11 +0100

    Update generate_all_plots.py

--- lysozyme/generate_all_plots.py
+++ lysozyme/generate_all_plots.py
@@ -17,18 +17,25 @@ s5=['1jto', '1jtp', '1jtt']
 g1a=['1ri8', '1bql',  '1zv5', '1mlc', '2iff']
 g1b=[ '1a2y', '1kiq', '1kip']
 g1c=['1jto', '1xfp']
-g2=['2dqd', '1xgr', '1xgt', '1xgq', '1dqj', '2dqc', '1c08', '1xgu', '1ic5', '1ic4', '1ic7', '2dqh', '1ua6', '1xgp', '2dqe', '2dqg']
-g1_mice= g2
+g22=['2dqd', '1xgr', '1xgt', '1xgq', '1dqj', '2dqc', '1c08', '1xgu', '1ic5', '1ic4', '1ic7', '2dqh', '1ua6', '1xgp', '2dqe', '2dqg']
+g1_mice= g22
 g2_mice= g1a
 g3_cs=['1jto','1jtp', '1jtt','1rjc','1xfp','1zmy','1t6v','1sq2','2i25','2i26']
-unique=['1a2y', '1bql', '1c08', '1dqj', '1dzb', '1ic4', '1ic5', '1ic7', '1jhl', '1kip', '1kiq', '1mlc', '1ndg', '1ndm', '1p2c', '1ua6', '1xgp', '1xgq', '1xgr', '1xgt', '1xgu', '2dqc', '2dqd', '2dqe', '2dqf', '2dqg', '2dqh', '2eiz', '2iff', '1jto', '1op9', '1ri8', '1xfp', '1zmy', '1zv5', '1zvh', '1zvy', '4i0c']
+g1=['2dqd', '1xgr', '1xgt', '1xgq', '1dqj', '2dqc', '1c08', '1xgu', '1ic5', '1ic4', '1ic7', '2dqh', '1ua6', '1xgp', '2dqe', '2dqg']
 
+c_and_m=['1jto', '1jtp', '1jtt', '1mel', '1op9', '1ri8', '1rjc', '1sq2', '1t6v', '1xfp', '1zmy', '1zv5', '1zvh', '1zvy', '2i25', '2i26', '3eba', '4i0c']
+m1=['2dqd', '1xgr', '1xgt', '1xgq', '1dqj', '2dqc', '1c08', '1xgu', '1ic5', '1ic4', '1ic7', '2dqh', '1ua6', '1xgp', '2dqe', '2dqg']
+m2=['1bql','1mlc', '2iff']
+cm=['1jto', '1jtp', '1jtt','1xfp','1zmy','2i25','2i26']
+cm2=['1zvy','1sq2','1t6v']
+cm3=['1ri8','1rjc','1zv5']
+unique=['1sq2','1t6v','2hfm','2i25','2i26','1a2y', '1bql', '1c08', '1dqj', '1dzb', '1ic4', '1ic5', '1ic7', '1jhl', '1kip', '1kiq', '1mlc', '1ndg', '1ndm', '1p2c', '1ua6', '1xgp', '1xgq', '1xgr', '1xgt', '1xgu', '2dqc', '2dqd', '2dqe', '2dqf', '2dqg', '2dqh', '2eiz', '2iff', '4tsa', '4tsb', '4tsc', '4ttd', '1jto', '1op9', '1ri8', '1xfp', '1zmy', '1zv5', '1zvh', '1zvy', '4i0c']
+unique1=['1a2y', '1c08', '1dqj', '1dzb', '1ic4', '1ic5', '1ic7', '1jhl', '1kip', '1kiq', '1mlc', '1ndg', '1ndm', '1p2c', '1ua6', '1xgp', '1xgq', '1xgr', '1xgt', '1xgu', '2dqc', '2dqd', '2dqe', '2dqf', '2dqg', '2dqh', '2eiz', '2iff', '1jto', '1op9', '1ri8', '1xfp', '1zmy', '1zv5', '1zvh', '1zvy', '4i0c']
+#removed  1bql from unique
 #removed 2hfm
 
-pos_paratopes=json.loads(open("equiv_contacts_dict_lsyozyme_pos_by_bonded.json").read())#dict type pos epitope:pos paratope
+unique_minus_1=set(unique).difference(set(g1))
 
-contacts1 =json.loads(open('equiv_contacts_dict_lsyozyme_heavy_h_bonded.json').read())
-contacts=json.loads(open('equiv_contacts_dict_lsyozyme_heavy_all.json').read())
 
 species=json.loads(open('species_lysozime.json').read())
 pdb_codes1= json.loads(open('pdb_codes_lysozyme.json').read())
@@ -57,7 +64,8 @@ def get_species(pdb):
     elif "human" in species[pdb]:
         sp+="H"
     return sp
-def get_json_dict(group, name):
+def get_json_dict(group, name, chain, type):
+    contacts =json.loads(open('equiv_contacts_dict_lsyozyme_pos_paratope_'+chain+'_'+type+'.json').read())
     g1={}
     g11={}
     for k, v in contacts.items():
@@ -108,6 +116,7 @@ def get_json_dict(group, name):
     return stackedd
 
 def get_heatmap_101(group, name, chain,type,size):
+    contacts=json.loads(open('equiv_contacts_dict_lsyozyme_pos_paratope_'+chain+'_'+type+'.json').read())
     common_pos=json.loads(open("common_pos_"+name+".json").read())
     d_g1={}
     a_dict={}
@@ -200,13 +209,12 @@ def run_code(group, name):
     get_heatmap_101(group, name)
 
 def get_heatmap_no_cont_para(group, name, chain, type,  size):
-
+    contacts1 =json.loads(open('equiv_contacts_dict_lsyozyme_pos_paratope_'+chain+'_'+type+'.json').read())
 
 
     cont_dict={}
-    for k, v in contacts.items():
+    for k, v in contacts1.items():
         for pdb in group:
-            d1={}
             if pdb in k:
                 cont_dict[pdb]={}
                 for pos, names in v.items():
@@ -235,9 +243,9 @@ def get_heatmap_no_cont_para(group, name, chain, type,  size):
 
 
 def get_heatmap_no_cont(group, name,chain, type, size,x):
-    #the_contacts=json.loads(open("equiv_contacts_dict_lsyozyme_pos_paratope_"+chain+"_"+ type+".json").read())
+    the_contacts=json.loads(open("equiv_contacts_dict_lsyozyme_pos_paratope_"+chain+"_"+ type+".json").read())
     cont_dict={}
-    for k, v in contacts.items():
+    for k, v in the_contacts.items():
         for pdb in group:
             d1={}
             if pdb in k:
@@ -261,17 +269,82 @@ def get_heatmap_no_cont(group, name,chain, type, size,x):
     plt.subplots_adjust(top=0.9) #adjust sublots labels to make room for title
     #plt.suptitle("Number of contacts - " + name, fontsize = 36) #title
     #colours=['#66c2a5','#386cb0','#8da0cb','#e78ac3','#a6d854','#ffd92f','#e5c494','#b3b3b3']
-    g = sns.heatmap(df, xticklabels=True, yticklabels=True, ax=ax, linewidths=.1, linecolor="black", annot=True, annot_kws={"size": 30,}, cmap="YlOrRd", cbar=False )#.set_title(chain+" chain: "+param)
-    ax.tick_params(labelbottom='off',labeltop='on', labelsize=30)
+    g = sns.heatmap(df, xticklabels=True, yticklabels=True, ax=ax, linewidths=.1, linecolor="black", annot=True, annot_kws={"size": 20,}, cmap="YlOrRd", cbar=False )#.set_title(chain+" chain: "+param)
+    ax.tick_params(labelbottom='off',labeltop='on', labelsize=20)
     plt.yticks(rotation=0)
     plt.xticks(rotation=90)
     ax.set_aspect("equal")
 
-    g.get_figure().savefig("epitope_no_contacts_"+name+"_"+chain+"_"+type+".png")
+    g.get_figure().savefig("new_epitope_no_contacts_"+name+"_"+chain+"_"+type+".png")
+    print(cont_dict)
 
+def get_heatmap_fw( group, name,chain, type, size, x):
+    cont_paratopes=json.loads(open('equiv_contacts_dict_lsyozyme_pos_paratope_'+chain+'_'+type+'.json').read())
+    #x= cont_paratopes[str(chain)+":"+"h_bonded"]
+    #y= epitope.get_contacts("heavy", "non_h_bonded")
+    #z= dict(y, **x)
+    #a= cont_paratopes[str(chain)+":"+"non_h_bonded"]
+    #b= epitope.get_contacts("light", "non_h_bonded")
+    #c=dict(b, **a)
+    #contact_dict = dict(x, **a)
+    #contacts_dict=dict(contact_dict, **a)
+
+    cont_dict={}
+    for k, v in cont_paratopes.items():
+        for pdb in group:
+            d1={}
+            if pdb in k:
+                cont_dict[pdb]={}
+                dicto={}
+                for pos, paratope_list in v.items():
+                    arg_list=[]
+                    dicto[pos]=[]
+                    unique_p_list=[]
+
+                    for paratope in paratope_list:
+                        if paratope not in unique_p_list:
+                            unique_p_list.append(paratope)
+                    for paratope in unique_p_list:
+                        try:
+                            if chain =="light":
+                                if int(paratope) in list(range(0,24)):
+                                    dicto[pos].append("FW11")
+                                elif int(paratope) in list(range(34,50)):
+                                    dicto[pos].append("FW2")
+                                elif int(paratope) in list(range(56,89)):
+                                    dicto[pos].append("FW3")
+                                elif int(paratope) >96:
+                                    dicto[pos].append("FW4")
+                            if chain=="heavy":
+                                if int(paratope) in list(range(0,26)):
+                                    dicto[pos].append("FW11")
+                                elif int(paratope) in list(range(32,52)):
+                                    dicto[pos].append("FW2")
+                                elif int(paratope) in list(range(56,95)):
+                                    dicto[pos].append("FW3")
+                                elif int(paratope) >103:
+                                    dicto[pos].append("FW4")
+
+
+                        except KeyError:
+                            continue
+                        cont_dict[pdb].update(dicto)
+
+
+    df3=pd.DataFrame.from_dict(cont_dict,orient='index')
+
+    df3.index.rename('PDB', inplace=True)
+    stacked = df3.stack().reset_index()
+    stacked.rename(columns={'level_1': 'Position', 0: 'FWR'}, inplace=True)
+    stacked=stacked.sort_values(by=['Position'])
+    stacked2=stacked.stack().reset_index()
+
+    c2=flatten_column(stacked, 'FWR')
+    df3.to_excel(name+"_cs_"+type+".xlsx")
+    return cont_dict
 
 def get_heatmap_CDR( group, name,chain, type, size, x):
-    cont_paratopes=contacts=json.loads(open('equiv_contacts_dict_lsyozyme_'+chain+'_all.json').read())
+    cont_paratopes=json.loads(open('equiv_contacts_dict_lsyozyme_pos_paratope_'+chain+'_'+type+'.json').read())
     #x= cont_paratopes[str(chain)+":"+"h_bonded"]
     #y= epitope.get_contacts("heavy", "non_h_bonded")
     #z= dict(y, **x)
@@ -338,10 +411,10 @@ def get_the_heatmap( group, name,chain, type, size, x):
 
     eq_dict_hm={}
     if chain=="heavy":
-        chain_type="H"
+        chain_type="FW"
     elif chain=="light":
-        chain_type="L"
-    for pdb, pos in get_heatmap_CDR( group, name,chain, type, size, x).items():
+        chain_type="FW"
+    for pdb, pos in get_heatmap_fw( group, name,chain, type, size, x).items():
         d_pos={}
         for p, cdr in pos.items():
             #eq_dict_hm[pdb][p]=[]
@@ -362,7 +435,7 @@ def get_the_heatmap( group, name,chain, type, size, x):
                 #    d_pos[p]=90
             if len(list(set(cdr)))==1:
                 if "o" in list(set(cdr)):
-                    d_pos[p]=8
+                    d_pos[p]=9
                 if chain_type+"1" in list(set(cdr)):
                     d_pos[p]=1
                 if chain_type+"2" in list(set(cdr)):
@@ -380,7 +453,7 @@ def get_the_heatmap( group, name,chain, type, size, x):
     stacked=stacked.sort_values(by=['Position'])
     maxim=int(stacked['Residues'].max())
     minim=int(stacked['Residues'].min())
-    cmap=['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5']
+    cmap=['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#bdbdbd']
     the_cmap=cmap[minim-1:maxim-1]
     fig, ax = plt.subplots(figsize=size)
     fig.autofmt_xdate()
@@ -392,7 +465,7 @@ def get_the_heatmap( group, name,chain, type, size, x):
     plt.xticks(rotation=90)
     ax.set_aspect("equal")
     #plt.show()
-    g.get_figure().savefig("CDR_heatmap_"+chain+"_"+name+"_"+type+".png")
+    g.get_figure().savefig("FWR_heatmap_"+chain+"_"+name+"_"+type+".png")
 
 
 def get_chain_letter(chain):
@@ -444,7 +517,8 @@ def sort_dict(d):
     for i in sorted(d):
         sort_dic.update({i:d[i]})
     return(sort_dic)
-def get_cdr_on_epitope(group,name, chain, type, size, x, the_cdr):
+
+def get_aa_residues_cdr(group,name, chain, type, size, x, the_cdr):
 
     cdr_range={"H1": list(range(26,33)),
                "H2": list(range(52, 57)),
@@ -454,8 +528,8 @@ def get_cdr_on_epitope(group,name, chain, type, size, x, the_cdr):
                "L3": list(range(89,98)),
                "o": [0,1]}
 
-    pos_res_paratopes=json.loads(open("equiv_contacts_dict_lsyozyme_pos_paratope_"+chain+"_all.json").read())
-    pos_name_paratope=json.loads(open("equiv_contacts_dict_lsyozyme_pos_name_paratope_"+chain+".json").read())
+    pos_res_paratopes=json.loads(open("equiv_contacts_dict_lsyozyme_pos_paratope_"+chain+"_"+type+".json").read())
+    pos_name_paratope=json.loads(open("equiv_contacts_dict_lsyozyme_pos_name_paratope_"+chain+"_"+type+".json").read())
 
     pos_res_paratope={}
     for pdb in pdb_codes1:
@@ -501,57 +575,60 @@ def get_cdr_on_epitope(group,name, chain, type, size, x, the_cdr):
                     aa_dict[cdr][pdb][pos]=aa_list[0]
                 else:
                     aa_dict[cdr][pdb][pos]=[]
+    return aa_dict
+def get_cdr_on_epitope(group,name, chain, type, size, x, the_cdr):
+
 
     aa_type_dict={}
     hm_aa_type_dict={}
-    for cdr, pdb_dict in aa_dict.items():
+    for cdr, pdb_dict in get_aa_residues_cdr(group,name, chain, type, size, x, the_cdr).items():
         aa_type_dict[cdr]={}
         hm_aa_type_dict[cdr]={}
         for pdb, pos_dict in pdb_dict.items():
-            aa_type_dict[cdr][pdb]={}
-            hm_aa_type_dict[cdr][pdb]={}
+            aa_type_dict[cdr][get_species(pdb)+"-"+pdb]={}
+            hm_aa_type_dict[cdr][get_species(pdb)+"-"+pdb]={}
             for pos, aa in pos_dict.items():
                 if aa in ["ASP", "GLU"]:
-                    aa_type_dict[cdr][pdb][pos]="bright_red"
-                    hm_aa_type_dict[cdr][pdb][pos]=1
+                    aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]="bright_red"
+                    hm_aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]=1
                 elif aa in ["CYS", "MET"]:
-                    aa_type_dict[cdr][pdb][pos]="yellow"
-                    hm_aa_type_dict[cdr][pdb][pos]=2
+                    aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]="yellow"
+                    hm_aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]=2
                 elif aa in ["LYS", "ARG"]:
-                    aa_type_dict[cdr][pdb][pos]="blue"
-                    hm_aa_type_dict[cdr][pdb][pos]=3
+                    aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]="blue"
+                    hm_aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]=3
                 elif aa in ["SER", "THR"]:
-                    aa_type_dict[cdr][pdb][pos]="orange"
-                    hm_aa_type_dict[cdr][pdb][pos]=4
+                    aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]="orange"
+                    hm_aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]=4
 
                 elif aa in ["PHE", "TYR"]:
-                    aa_type_dict[cdr][pdb][pos]="mild_blue"
-                    hm_aa_type_dict[cdr][pdb][pos]=5
+                    aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]="mild_blue"
+                    hm_aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]=5
                 elif aa in ["ASN", "GLN"]:
-                    aa_type_dict[cdr][pdb][pos]="cyan"
-                    hm_aa_type_dict[cdr][pdb][pos]=6
+                    aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]="cyan"
+                    hm_aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]=6
                 elif aa in ["Leu", "VAL", "ILE"]:
-                    aa_type_dict[cdr][pdb][pos]="green"
+                    aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]="green"
                 elif aa in ["GLY"]:
-                    hm_aa_type_dict[cdr][pdb][pos]=7
-                    aa_type_dict[cdr][pdb][pos]="light_gray"
+                    hm_aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]=7
+                    aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]="light_gray"
                 elif aa in ["ALA"]:
-                    aa_type_dict[cdr][pdb][pos]="dark_gray"
-                    hm_aa_type_dict[cdr][pdb][pos]=8
+                    aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]="dark_gray"
+                    hm_aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]=8
                 elif aa in ["TRP"]:
-                    aa_type_dict[cdr][pdb][pos]="pink"
-                    hm_aa_type_dict[cdr][pdb][pos]=9
+                    aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]="pink"
+                    hm_aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]=9
                 elif aa in ["HIS"]:
-                    aa_type_dict[cdr][pdb][pos]="pale_blue"
-                    hm_aa_type_dict[cdr][pdb][pos]=10
+                    aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]="pale_blue"
+                    hm_aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]=10
                 elif aa in ["PRO"]:
-                    aa_type_dict[cdr][pdb][pos]="flesh"
-                    hm_aa_type_dict[cdr][pdb][pos]=11
+                    aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]="flesh"
+                    hm_aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]=11
                 else:
-                    aa_type_dict[cdr][pdb][pos]="black"
-                    hm_aa_type_dict[cdr][pdb][pos]=12
+                    aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]="black"
+                    hm_aa_type_dict[cdr][get_species(pdb)+"-"+pdb][pos]=12
 
-    df_aa=pd.DataFrame.from_dict(aa_dict[the_cdr], orient="Index")
+    df_aa=pd.DataFrame.from_dict(aa_type_dict[the_cdr], orient="Index")
     df_aa.index.rename('PDB', inplace=True)
     stacked = df_aa.stack().reset_index()
     stacked.rename(columns={'level_1': 'Position', 0: 'Residues'}, inplace=True)
@@ -587,23 +664,26 @@ def get_cdr_on_epitope(group,name, chain, type, size, x, the_cdr):
 
 
 
-def get_heatmap_hydrophobicity(group, name):
-    cont_dict={}
-    for k, v in contacts.items():
-        for pdb in group:
-            d1={}
-            if pdb in k:
-                cont_dict[pdb]={}
-                for pos, names in v.items():
-                    for res in names:
-                        try:
-                            dict={pos: len(names)}
+def get_heatmap_aa_type_cdr(group,name, chain, type, size, x, the_cdr):
+    hm_aa_type_dict=get_aa_residues_cdr(group,name, chain, type, size, x, the_cdr)
 
+    df_aa=pd.DataFrame.from_dict(hm_aa_type_dict[the_cdr], orient="Index")
+    df_aa.index.rename('PDB', inplace=True)
+    stacked = df_aa.stack().reset_index()
+    stacked.rename(columns={'level_1': 'Position', 0: 'Residues'}, inplace=True)
+    stacked=stacked.sort_values(by=['Position'])
+    stacked2=stacked.stack().reset_index()
 
-                        except KeyError:
-                            continue
-                        cont_dict[pdb].update(dict)
+    #b=sns.scatterplot(data=stacked, x='Position', y='PDB', hue='Residues')
+    #plt.show()
+    cmap2=[(230,10,10),(230,230,0),(20,90,255), (250,150,0),(50,50,170),(0,220,220),(15,130,15), (235,235,235),(200,200,200),(180,90,180),(130,130,210),(220,150,130)]
+    cmap=['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#66c2a5','#fc8d62','#8da0cb','#e78ac3','#a6d854','#ffd92f','#e5c494','#7fc97f','#beaed4','#fdc086','#ffff99','#386cb0','#f0027f','#bf5b17']
+    df_aa=df_aa[natsorted(df_aa.columns)]
+    stacked_hm = df_aa.stack().reset_index()
+    stacked_hm.rename(columns={'level_1': 'Position', 0: 'Residues'}, inplace=True)
+    counts =df_aa.apply(pd.Series.value_counts, axis=1)
 
+    return counts
 """
 run_code(pdb_codes1, "all_codes")
 
@@ -643,6 +723,7 @@ cpd=pd.DataFrame.from_dict(common_pos,orient="index")
 """
 """
 groups= [(g1_mice, "g1_mice"), (g2_mice, "g2_mice"), (g3_cs, "g3_cs"), (unique, "unique_all_pdbs")]
+#        for cdr in [str(get_chain_letter(chain))+'1', str(get_chain_letter(chain))+'2',str(get_chain_letter(chain))+'3',"o"]:
 for chain in ["light"]:
     for (a, b) in groups:
         x=5
@@ -696,13 +777,17 @@ for chain in ["light"]:
 
 
 """
-groups= [(g1_mice, "g1_mice"), (g2_mice, "g2_mice"), (g3_cs, "g3_cs"), (unique, "unique_all_pdbs")]
-for cdr in [str(get_chain_letter(chain))+'1', str(get_chain_letter(chain))+'2',str(get_chain_letter(chain))+'3',"o"]:
-    try:
-        get_the_heatmap(a, b, chain, "all", (30,30), 5)
-    except IndexError:
-        continue
 
-
-
+groups= [(cm,"cm1"),(cm2,"cm2"),(cm3, "cm3"), (m1, "m1"),(m2,"m2")]
+for chain in ["heavy"]:
+    for cdr in [str(get_chain_letter(chain))+'1', str(get_chain_letter(chain))+'2',str(get_chain_letter(chain))+'3',"o"]:
+        for (a,b) in groups:
+            try:
+                get_cdr_on_epitope(a, b, chain, "all",(20,20), 5, cdr)
+            except IndexError:
+                continue
+"""               
+with open('aa_type_frequency_heavy.json', 'w') as ctr: #save the contact residues in a json file
+    json.dump(get_heatmap_aa_type_cdr(unique, "unique_all_pdbs", "heavy", "all", (5,5), 5, "H3"), ctr)
+"""
 
