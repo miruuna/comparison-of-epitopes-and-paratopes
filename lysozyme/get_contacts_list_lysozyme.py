52bbd954b685645f9ae7118a5fe672e9d9f7c291
Miruna Serian <zcbtser@ucl.ac.uk>
2019-09-02 16:46:25 +0100

    Update get_contacts_list_lysozyme.py

--- lysozyme/get_contacts_list_lysozyme.py
+++ lysozyme/get_contacts_list_lysozyme.py
@@ -1,4 +1,3 @@
-
 import json
 import pandas as pd
 import seaborn as sns
@@ -8,6 +7,7 @@ import matplotlib.pyplot as plt
 annotation = json.loads(open('data_lysozyme.json').read())
 pdb_codes1=["1tzg"]
 pdb_codes= json.loads(open('pdb_codes_lysozyme.json').read())
+species=json.loads(open('species_lysozime.json').read())
 contact_residues= json.loads(open('contact_residues_all_imgtt.json').read())
 
 def get_contacts(chain, param):
@@ -16,35 +16,67 @@ def get_contacts(chain, param):
         a_dict={}
         pdb_light_=[]
         entry_list=[]
-        antigen = annotation[pdb]['antigen'][0]
+
+        for e in annotation[pdb]['antigen']:
+            if e in ['C','Y','L']:
+                antigen=e
+            else:
+                antigen=annotation[pdb]['antigen'][0]
+
+
         #antibody= annotation[pdb]['antibody']
+
         if chain=="light":
             if len(annotation[pdb]['antibody'])>=2:
-                if (elem=='L' for elem in annotation[pdb]['antibody']):
-                    antibody= annotation[pdb]['antibody'][1]
-                    non_h_cons=contact_residues[pdb][antigen][antibody][param]
+                if pdb in ['1jto', '1jtt', '1jtp']:
+                    non_h_cons=[]
+
+                else:
+                    for elem in annotation[pdb]['antibody']:
+                        if elem=='L':
+                            non_h_cons=contact_residues[pdb][antigen][elem][param]
+                        if elem=='A':
+                            non_h_cons=contact_residues[pdb][antigen][elem][param]
+
 
             else:
-                antibody= annotation[pdb]['antibody'][0]
                 non_h_cons=[]
 
         elif chain=="heavy":
             if len(annotation[pdb]['antibody'])>=2:
-                if (elem=='H' for elem in annotation[pdb]['antibody']):
-                    antibody= annotation[pdb]['antibody'][0]
-                    non_h_cons=contact_residues[pdb][antigen][antibody][param]
-                elif (elem=='B' for elem in annotation[pdb]['antibody']):
-                    antibody= annotation[pdb]['antibody'][1]
-                    non_h_cons=contact_residues[pdb][antigen][antibody][param]
+                if pdb in ['1jto', '1jtt', '1jtp']:
+                    non_h_cons=contact_residues[pdb]['L']['A'][param]
+                    if non_h_cons==[]:
+                        non_h_cons=contact_residues[pdb]['M']['B'][param]
+                else:
+                    for elem in annotation[pdb]['antibody']:
+                        if str(elem) =='H':
+                            non_h_cons=contact_residues[pdb][antigen][elem][param]
+                        elif elem=='B':
+                            non_h_cons=contact_residues[pdb][antigen][elem][param]
+                        elif elem=='N':
+                            non_h_cons=contact_residues[pdb][antigen][elem][param]
+
             elif len(annotation[pdb]['antibody'])==1:
                 antibody= annotation[pdb]['antibody'][0]
                 non_h_cons=contact_residues[pdb][antigen][antibody][param]
 
         else:
             print("no chain given")
-        res1_pos=[]
+        res1_pos={}
+        pos1=[]
         for elem in non_h_cons:
-            res1_pos.append(elem['res1_pos'])
+            if elem not in pos1:
+                pos1.append(elem['res1_pos'])
+                pos=sorted(pos1)
+
+        for res in pos:
+            res1_pos[res]=[]
+            for elem in non_h_cons:
+                if res==elem['res1_pos']:
+                    res1_pos[res].append(elem['res1_name'])
+
+
             no_elem=non_h_cons.count(elem)
         #dict={pos: int(res1_pos.count(pos)) for pos in res1_pos}
         #heavy_chain[pdb]=(res1_pos)
@@ -58,5 +90,6 @@ def get_contacts(chain, param):
 
 
     return contacts
+print(get_contacts('light', 'h_bonded'))
 
-print(get_contacts('heavy','non_h_bonded')['1sq2'])
+
